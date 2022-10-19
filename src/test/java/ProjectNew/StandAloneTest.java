d76abde7753472f57371ec703afe255053d94609
vinay <vinaynagawade@gmail.com>
2022-10-19 12:38:18 +0530

    fi

--- src/test/java/ProjectNew/StandAloneTest.java
+++ src/test/java/ProjectNew/StandAloneTest.java
@@ -14,11 +14,12 @@ import ProjectNew.baseComponent.Retry;
 
 public class StandAloneTest extends BaseClass {
 
-	 String expectedMassege = "THANKYOU FOR THE ORDER.";
-	 String contry = "India";
+	@Test(retryAnalyzer=Retry.class)
+	public  void abc() throws IOException, InterruptedException {
+	}
 	 
 	 
-	/*@Test
+	@Test(retryAnalyzer=Retry.class)
 	public  void cheakBorrowIstimate() throws IOException, InterruptedException {
 		
 		
@@ -26,25 +27,25 @@ public class StandAloneTest extends BaseClass {
 		WebElement select =landingPage.selectNoOfDependant();
 		Select s = new Select(select);
 		s.selectByVisibleText("0");
-		landingPage.homeToLiveIn();
-		landingPage.annualIncom(80000);
-		landingPage.annualOtherIncom(10000);
-		landingPage.livingExpenses(500);
-		landingPage.homeLoanmonthlyRepayment(0);
-		landingPage.monthlyOtherLoanRepayment(100);
-		landingPage.monthlyOtherComitment(0);
-		landingPage.creditCardLimit(10000);
+		landingPage.homeToLiveIn("singal");
+		landingPage.annualIncom("80000");
+		landingPage.annualOtherIncom("10000");
+		landingPage.livingExpenses("500");
+		landingPage.homeLoanmonthlyRepayment("0");
+		landingPage.monthlyOtherLoanRepayment("100");
+		landingPage.monthlyOtherComitment("0");
+		landingPage.creditCardLimit("10000");
 		landingPage.clickOnBorrowCalculatorBtn();
 		String v=landingPage.estimateYouBorrow();
 		Assert.assertEquals(v, "$419,000");
 		landingPage.clickOnStartOver();
-		landingPage.livingExpenses(1);
+		landingPage.livingExpenses("1");
 		landingPage.clickOnBorrowCalculatorBtn();
 		String errorMassage =landingPage.errorMassege(); 
 		Assert.assertEquals(errorMassage, "Based on the details you've entered, we're unable to give you an estimate of your borrowing power with this calculator. For questions, call us on 1800 035 500.");  
 		
 		
-	}*/
+	}
 	
 	
 	
